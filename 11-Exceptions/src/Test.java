b997910f5dd069451b1911f939927afa5f59a28f
savychev <werk3093059@gmail.com>
2025-08-14 13:19:56 +0200

    oefenen

--- 11-Exceptions/src/Test.java
+++ 11-Exceptions/src/Test.java
@@ -1,3 +1,9 @@
 public class Test {
+    public static void main(String[] args) {
+//        String[] words = {"hi", null};
+//        System.out.println(words[1].length());
 
+        int[][] matrix = new int[3][];
+        System.out.println(matrix[0].length);
+    }
 }
