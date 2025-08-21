5919a1abe5f6764cd0da399ec2102a7c2039c26d
savychev <werk3093059@gmail.com>
2025-08-21 15:51:15 +0200

    oefenen

--- 11-Exceptions/src/Test.java
+++ 11-Exceptions/src/Test.java
@@ -1,10 +1,18 @@
 public class Test {
     public static void main(String[] args) {
-        byte x = 5;
-        byte y = 10;
-        float z = x + y;
+        int count = 0;
 
+        ROW_LOOP:
+        for (int row = 1; row <= 3; row++)
+            for (int col = 1; col <= 2; col++) {
+                if (row * col % 2 == 0) {
+                    System.out.println("count = " + count + "\nrow = " + row + "\ncol = " + col + "\n");
+                    continue ROW_LOOP;
+                }
+                count++;
+            }
 
-        System.out.println(z);
+        System.out.println(count);
+
     }
 }
