6fe34f5d05a04ae95b45bcd90b46c0fcf8db0440
savychev <werk3093059@gmail.com>
2025-08-26 15:55:04 +0200

    oefenen

--- 11-Exceptions/src/Test.java
+++ 11-Exceptions/src/Test.java
@@ -1,18 +1,16 @@
+import java.util.Arrays;
+
+
 public class Test {
+    public static int howMany(boolean b, boolean... b2) {
+        System.out.println("b2 = " + Arrays.toString(b2));
+        return b2.length;
+    }
+
     public static void main(String[] args) {
-        int count = 0;
-
-        ROW_LOOP:
-        for (int row = 1; row <= 3; row++)
-            for (int col = 1; col <= 2; col++) {
-                if (row * col % 2 == 0) {
-                    System.out.println("count = " + count + "\nrow = " + row + "\ncol = " + col + "\n");
-                    continue ROW_LOOP;
-                }
-                count++;
-            }
-
-        System.out.println(count);
-
+        System.out.println("C: " + howMany(true, true));
+        System.out.println("D: " + howMany(true, true, true));
+        System.out.println("G: " + howMany(true, new boolean[2]));
     }
 }
+
